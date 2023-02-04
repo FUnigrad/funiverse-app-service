e8fb2bf4503d8c03b8a09520dddd3431e996b6d9
uyentrang2247 <truongthiuyentrang@gmail.com>
2023-02-04 12:18:54 +0700

    Initial Project

---
+++ src/main/java/com/unigrad/funiverseappservice/ApplicationController.java
@@ -0,0 +1,18 @@
+package com.unigrad.funiverseappservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+    @GetMapping
+    public ResponseEntity<String> getStringHello() throws UnknownHostException {
+        return ResponseEntity.ok("Application: "+ InetAddress.getLocalHost().getHostName());
+    }
+}
---
+++ src/test/java/com/unigrad/funiverseappservice/FuniverseAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unigrad.funiverseappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuniverseAppServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
