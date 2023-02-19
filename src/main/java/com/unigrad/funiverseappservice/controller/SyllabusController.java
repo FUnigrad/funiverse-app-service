b73b18215d70ec4f0430cbbb2f577aaa1331121e
Lomahs <58368317+lomahs@users.noreply.github.com>
2023-02-19 11:31:10 +0700

    FUN-124 BE - Admin CRUD Syllabus (#4)

---
+++ src/main/java/com/unigrad/funiverseappservice/controller/SyllabusController.java
@@ -0,0 +1,84 @@
+package com.unigrad.funiverseappservice.controller;
+
+import com.unigrad.funiverseappservice.entity.academic.Syllabus;
+import com.unigrad.funiverseappservice.service.ISyllabusService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("syllabus")
+public class SyllabusController {
+
+    private final ISyllabusService syllabusService;
+
+    public SyllabusController(ISyllabusService syllabusService) {
+        this.syllabusService = syllabusService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Syllabus>> getAll(@RequestParam(required = false) String code) {
+
+        List<Syllabus> syllabi = code == null
+                ? syllabusService.getAll() : List.of();
+
+        return ResponseEntity.ok(syllabi);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Syllabus> getById(@PathVariable Long id) {
+
+        return syllabusService.get(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody Syllabus syllabus) {
+        Syllabus newSyllabus = syllabusService.save(syllabus);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newSyllabus.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Syllabus> update(@RequestBody Syllabus syllabus) {
+
+        return syllabusService.isExist(syllabus.getId())
+                ? ResponseEntity.ok(syllabusService.save(syllabus))
+                : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Syllabus> deactivate(@PathVariable Long id) {
+        syllabusService.deactivate(id);
+
+        return syllabusService.get(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Syllabus> activate(@PathVariable Long id) {
+        syllabusService.activate(id);
+
+        return syllabusService.get(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/com/unigrad/funiverseappservice/service/impl/SyllabusService.java
@@ -0,0 +1,57 @@
+package com.unigrad.funiverseappservice.service.impl;
+
+import com.unigrad.funiverseappservice.entity.academic.Syllabus;
+import com.unigrad.funiverseappservice.repository.ISyllabusRepository;
+import com.unigrad.funiverseappservice.service.ISyllabusService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SyllabusService implements ISyllabusService {
+
+    private final ISyllabusRepository syllabusRepository;
+
+    public SyllabusService(ISyllabusRepository syllabusRepository) {
+        this.syllabusRepository = syllabusRepository;
+    }
+
+    @Override
+    public List<Syllabus> getAll() {
+        return syllabusRepository.findAll();
+    }
+
+    @Override
+    public List<Syllabus> getAllActive() {
+        return syllabusRepository.findAllByActiveIsTrue();
+    }
+
+    @Override
+    public Optional<Syllabus> get(Long key) {
+        return syllabusRepository.findById(key);
+    }
+
+    @Override
+    public Syllabus save(Syllabus entity) {
+        return syllabusRepository.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public void activate(Long key) {
+        syllabusRepository.updateIsActive(key, true);
+    }
+
+    @Override
+    @Transactional
+    public void deactivate(Long key) {
+        syllabusRepository.updateIsActive(key, false);
+    }
+
+    @Override
+    public boolean isExist(Long key) {
+        return syllabusRepository.existsById(key);
+    }
+}
