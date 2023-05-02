50a15ea6ed88ea24e6e6f6d86c49ab99c53c434b
Lomahs <58368317+lomahs@users.noreply.github.com>
2023-05-02 13:40:23 +0700

    chore: Remove redundant data

--- src/main/resources/data/season.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO season (id, end_month, is_active, name, ordinal_number, start_month)
-VALUES  (1, 4, true, 'SPRING', 1, 1),
-        (2, 8, true, 'SUMMER', 2, 5),
-        (3, 12, true, 'FALL', 3, 9);
